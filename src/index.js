cc04dcc5d72a8da298297a8444321e393a1b19e7
raylin009 <raylin009@gmail.com>
2022-06-08 10:47:41 -0700

    renvering moves button, jumpTo not implimented

--- src/index.js
+++ src/index.js
@@ -82,6 +82,17 @@ class Game extends React.Component {
     const history = this.state.history;
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
+
+    const moves = history.map((step, move)=>{
+      const desc = move?
+        'Go to move #' + move:
+        'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jumpTp(move)}>{desc}</button>
+        </li>
+      )
+    })
     let status;
     if(winner){
       status = 'Winner is: ' + winner;
@@ -100,7 +111,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
